275cbc3e79a73d54e4bb5796c9193bdf2ffeca4e
VK <vit_ko@ukr.net>
2025-04-18 15:39:06 +0200

    Start the 'Tic Tac Toe' project. Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic Tac Toe
+
+This simple and familiar game was created with React.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hallo tic-tac-toe</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
